556ddccdfa67d55047bbf8091745c878c9afd1de
nryberg <nryberg@gmail.com>
2025-12-31 13:22:56 -0600

    Different styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,16 @@
 module.exports = function(eleventyConfig) {
+  // Copy CSS files to output
+  eleventyConfig.addPassthroughCopy("src/css");
+
+  // Copy any images or other static assets
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  // Add global data for style selection
+  eleventyConfig.addGlobalData("styleFile", () => {
+    const style = process.env.STYLE || "1";
+    return `style${style}.css`;
+  });
+
   // Return your Object options:
   return {
     dir: {
